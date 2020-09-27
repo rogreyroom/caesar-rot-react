a5842a32771d6acaeec0a25077807d5eb3973266
Robert Adamczewski <robert@adamczewski.me>
2020-09-27 12:29:31 +0200

    feat: change default app render to serve the new app components

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import styled from 'styled-components'
+import Main from './app/components/Main';
+import BottomSvg from './app/components/BottomSvg'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+    <AppBody>
+      <Main/>
+      <BottomSvg/>
+    </AppBody>
+  )
 }
 
-export default App;
+const AppBody = styled.div`
+background: var(--bg-gradient);
+	min-height: 100vh;
+	margin: 0;
+	padding: 0;
+	display: flex;
+  overflow: hidden;
+  position: relative;
+  z-index: 1;
+`
+
+export default App
