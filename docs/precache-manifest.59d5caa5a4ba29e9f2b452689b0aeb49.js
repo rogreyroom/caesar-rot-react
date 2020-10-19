97c76c60e7eab78474e9676aa292c67fbbf8c494
Robert Adamczewski <robert@adamczewski.me>
2020-10-19 18:11:18 +0200

    feat: new build

---
+++ docs/precache-manifest.59d5caa5a4ba29e9f2b452689b0aeb49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b9602e09fac78351100a1cb1cbb655",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e01da5a2aeaa300fa606",
+    "url": "./static/css/main.4d298151.chunk.css"
+  },
+  {
+    "revision": "3a93d79a51f99dd7d597",
+    "url": "./static/js/2.3e1e50a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.3e1e50a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e01da5a2aeaa300fa606",
+    "url": "./static/js/main.843cc16a.chunk.js"
+  },
+  {
+    "revision": "6b5c1e1bb6526bdfa129",
+    "url": "./static/js/runtime-main.2d0e95d3.js"
+  },
+  {
+    "revision": "79e917ace78fe9515148d18f30105806",
+    "url": "./static/media/julius-caesar.79e917ac.png"
+  }
+]);
